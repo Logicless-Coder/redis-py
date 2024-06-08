7e746d3149ca42720afa5c07281c47a4c0a43ae6
Abhishek Rathore <abhirathore565@gmail.com>
2024-06-08 19:53:34 +0530

    RESP Serialization implemented

--- redis_py/main.py
+++ redis_py/main.py
@@ -45,3 +45,46 @@ class RESP:
             return array
         else:
             raise NotImplementedError
+
+    def serialize(
+        self,
+        command: str | int | None | list[str | int | None],
+        error: bool = False,
+        bulk: bool = False,
+    ) -> str:
+        if command is None:
+            return self._serialize_none()
+        elif isinstance(command, int):
+            return self._serialize_int(command)
+        elif isinstance(command, str):
+            if bulk:
+                return self._serialize_bulk(command)
+            else:
+                return self._serialize_string(command, error=error)
+        elif isinstance(command, list):
+            array = []
+            for item in command:
+                array.append(self.serialize(item, error=error, bulk=bulk))
+            print(f"{array=}")
+
+            return self._serialize_array(array)
+        else:
+            raise NotImplementedError
+
+    def _serialize_none(self) -> str:
+        return "$-1" + self.CRLF
+
+    def _serialize_int(self, x: int) -> str:
+        return ":" + str(x) + self.CRLF
+
+    def _serialize_string(self, s: str, error: bool = False) -> str:
+        if error:
+            return "-" + s + self.CRLF
+        else:
+            return "+" + s + self.CRLF
+
+    def _serialize_bulk(self, s: str) -> str:
+        return "$" + str(len(s)) + self.CRLF + s + self.CRLF
+
+    def _serialize_array(self, a: list) -> str:
+        return "*" + str(len(a)) + self.CRLF + "".join(a)
--- tests/test_resp.py
+++ tests/test_resp.py
@@ -63,3 +63,55 @@ class TestRESPDeserialization:
         assert len(command) == 2
         assert command[0] == "get"
         assert command[1] == "key"
+
+
+class TestRESPSerialization:
+    resp = RESP()
+
+    def test_null_bulk_string(self):
+        command = None
+        raw = self.resp.serialize(command)
+
+        assert raw == "$-1\r\n"
+
+    def test_simple_string(self):
+        command = "OK"
+        raw = self.resp.serialize(command)
+
+        assert raw == "+OK\r\n"
+
+    def test_simple_error(self):
+        command = "Error message"
+        raw = self.resp.serialize(command, error=True)
+
+        assert raw == "-Error message\r\n"
+
+    def test_simple_string_2(self):
+        command = "hello world"
+        raw = self.resp.serialize(command)
+
+        assert raw == "+hello world\r\n"
+
+    def test_empty_bulk_string(self):
+        command = ""
+        raw = self.resp.serialize(command, bulk=True)
+
+        assert raw == "$0\r\n\r\n"
+
+    def test_array_single_string(self):
+        command = ["ping"]
+        raw = self.resp.serialize(command, bulk=True)
+
+        assert raw == "*1\r\n$4\r\nping\r\n"
+
+    def test_array_double_string(self):
+        command = ["echo", "hello world"]
+        raw = self.resp.serialize(command, bulk=True)
+
+        assert raw == "*2\r\n$4\r\necho\r\n$11\r\nhello world\r\n"
+
+    def test_array_double_string_2(self):
+        command = ["get", "key"]
+        raw = self.resp.serialize(command, bulk=True)
+
+        assert raw == "*2\r\n$3\r\nget\r\n$3\r\nkey\r\n"
